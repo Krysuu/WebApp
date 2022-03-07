229a145a5e2185b274ae41128791536d3d8c5d67
Krystian Nowakowski <krystiann.nowakowski@gmail.com>
2022-03-07 17:39:12 +0100

    Cleanup hello world

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -8,16 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
 @SpringBootApplication
-@Theme(value = "mytodo")
-@PWA(name = "My Todo", shortName = "My Todo", offlineResources = {"images/logo.png"})
+@Theme(value = "webapp")
+@PWA(name = "WebApp", shortName = "WebApp", offlineResources = {"images/logo.png"})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
